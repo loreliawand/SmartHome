2c8000f857790357808e59bc8a9b37ae2660b06b
loreliawand <loreliawand@gmail.com>
2022-02-06 17:26:20 +0100

    Created very basic construction of page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome, user_name!</h1>
+      <button>Synchronization</button>
+      <br></br>
+      <br></br>
+      <button>Settings</button>
+      <br></br>
+      <br></br>
+      <button>Sort by default</button>
+      <div>
+        <h2 className="device">Bulb</h2>
+      </div>
+      <div>
+        <h2 className="device">Outlet</h2>
+      </div>
+      <div>
+        <h2 className="device">Temperature sensor</h2>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
