37ffc5f869b21cbb3d49c524c5a244b1da424d02
loreliawand <loreliawand@gmail.com>
2022-02-07 11:03:32 +0100

    Logic for showing devices and pseudodb

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-const App = () => {
+const App = (props) => {
+  const { devices } = props;
+
+  const clicking = () => {
+    console.log('clicked!');
+  };
   return (
     <div>
       <h1>Welcome, user_name!</h1>
@@ -9,15 +14,18 @@ const App = () => {
       <br></br>
       <br></br>
       <button>Sort by default</button>
-      <div>
-        <h2 className="device">Bulb</h2>
-      </div>
-      <div>
-        <h2 className="device">Outlet</h2>
-      </div>
-      <div>
-        <h2 className="device">Temperature sensor</h2>
-      </div>
+      {devices.map((device) => (
+        <div key={device.id} className="device" onClick={clicking}>
+          <h2>{device.name}</h2>
+          <span>{device.id}</span>
+          <br></br>
+          <span>{device.name}</span>
+          <br></br>
+          <span>{device.type}</span>
+          <br></br>
+          <span>{device.connectionState}</span>
+        </div>
+      ))}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,4 +2,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devices = [
+  {
+    type: 'bulb',
+    id: 1,
+    name: 'newsun_2022',
+    connectionState: 'connected',
+    isTurnedOn: true,
+    brightness: 95,
+    color: 'white',
+  },
+  {
+    type: 'outlet',
+    id: 2,
+    name: 'tunnel_2022',
+    connectionState: 'disconnected',
+    isTurnedOn: false,
+    powerConsumpction: 0,
+  },
+  {
+    type: 'temperatureSensor',
+    id: 3,
+    name: 'climate_2022',
+    connectionState: 'poorConnectiont',
+    temperature: 0,
+  },
+];
+
+ReactDOM.render(<App devices={devices} />, document.getElementById('root'));
